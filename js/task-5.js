27585c41d67ec9849172f1e6829269c3a1149a72
Roman Dobrodii <romariosi007@gmail.com>
2020-10-04 16:04:54 +0300

    Добавляет решение 5-7 задачи

--- js/task-5.js
+++ js/task-5.js
@@ -1,3 +1,15 @@
 // Напиши скрипт который, при наборе текста в инпуте input#name - input(событие input),
 // подставляет его текущее значение в span#name - output. Если инпут пустой,
 // в спане должна отображаться строка 'незнакомец'.
+
+const inputNameRef = document.querySelector('input#name-input');
+const outputNameRef = document.querySelector('span#name-output');
+
+const onInputChange = event => {
+  outputNameRef.textContent = event.currentTarget.value;
+  if (event.currentTarget.value === '') {
+    outputNameRef.textContent = 'незнакомец';
+  }
+};
+
+inputNameRef.addEventListener('input', onInputChange);
